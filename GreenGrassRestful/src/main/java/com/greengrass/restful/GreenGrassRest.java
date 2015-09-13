2444726321b8d94cbfe7d6424801e86b30186ff4
user <user@pc>
2015-09-13 10:55:37 +0700

    ede

---
+++ GreenGrassRestful/src/main/java/com/greengrass/restful/GreenGrassRest.java
@@ -0,0 +1,23 @@
+package com.greengrass.restful;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@RestController
+public class GreenGrassRest {
+
+    @RequestMapping("/test")
+    public String test(@RequestParam(value="name", defaultValue="World") String name) {
+	    //GcApp app = new GcApp();
+
+	    String testStr = "Колбасная продукция!22222 test! ";
+        GsonBuilder builder = new GsonBuilder();    
+        Gson gson = builder.create();
+        return gson.toJson(testStr);
+    }
+    
+}
