f8586083dad9f0d20eb7048f059cc852b0abe0b4
user <user@pc>
2015-09-13 17:13:43 +0700

    dd

--- GreenGrassRestful/src/main/java/com/greengrass/restful/App.java
+++ GreenGrassRestful/src/main/java/com/greengrass/restful/App.java
@@ -1,12 +1,16 @@
 package com.greengrass.restful;
 
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.greengrass.house.AnyApp;
+import com.greengrass.house.GGException;
 
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
--- GreenGrassRestful/src/main/java/com/greengrass/restful/GreenGrassRest.java
+++ GreenGrassRestful/src/main/java/com/greengrass/restful/GreenGrassRest.java
@@ -1,23 +1,37 @@
 package com.greengrass.restful;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.greengrass.house.GGException;
+import com.greengrass.house.Obj;
 
 @RestController
 public class GreenGrassRest {
 
     @RequestMapping("/test")
     public String test(@RequestParam(value="name", defaultValue="World") String name) {
-	    //GcApp app = new GcApp();
+    	RestApp app = null;
+    	try {
+			app = new RestApp("GGE");
+		} catch (GGException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+    	app.beginTrans();
+		List<Obj> lobj = app.session.createQuery(
+				"from Obj where cd not in ('GGE','GGH','GGV')").list(); 
 
-	    String testStr = "Колбасная продукция!22222 test! ";
+	    //String testStr = "Колбасная продукция!22222 test! ";
         GsonBuilder builder = new GsonBuilder();    
         Gson gson = builder.create();
-        return gson.toJson(testStr);
+        return gson.toJson(lobj.toString());
     }
     
 }
