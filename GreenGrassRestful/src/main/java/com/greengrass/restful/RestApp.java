7060c63b1c73f3cf1d65a7f5eb34d2e2846c996e
user <user@pc>
2015-09-14 18:43:43 +0700

    h

---
+++ GreenGrassRestful/src/main/java/com/greengrass/restful/RestApp.java
@@ -0,0 +1,12 @@
+package com.greengrass.restful;
+
+import com.greengrass.house.AnyApp;
+import com.greengrass.house.GGException;
+
+public class RestApp extends AnyApp {
+
+	public RestApp(String appCd) throws GGException {
+		super(appCd);
+	}
+
+}
