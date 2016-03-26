9ca44503b9e126b9cbe9efcce1beb641094fcd2a
user <user@pc>
2016-03-26 09:29:27 +0700

    на утро

--- Direct/src/main/java/com/direct/webflow/ThrMain.java
+++ Direct/src/main/java/com/direct/webflow/ThrMain.java
@@ -3,91 +3,26 @@ package com.direct.webflow;
 import java.util.List;
 
 
-public class ThrMain<SprGenItm> extends Thread {
-	   private Thread tgh1, tgh2, tgh3, tgh4, tgh5;
+public class ThrMain extends Thread {
+	   private ThrGen tgh1, tgh2, tgh3, tgh4, tgh5;
 	   private List<SprGenItm> sprg;
 	   public boolean stopped=false;
 	   
 	   ThrMain(List<SprGenItm> sprg){
 	       this.sprg=sprg;
-	       System.out.println("Creating ThrMain!");
+		   System.out.println("Creating ThrMain!");
 	   }
 	   
 	   public void run() {
-		   
 	   
 		   for (SprGenItm itm : this.sprg) {
 			   //этот поток должен создать объект содержащий дома которые нужно сформировать, потоки ThrGen должны "разбирать" у этого объекта дома для формир. 
 			   System.out.println("Generating menu item: "+itm.getCd());
-			   
-
-			   tgh1= new ThrGen("HOUSE Thread-1");
-			   tgh1.start();
-			   
-			   tgh3= new ThrGen("HOUSE Thread-3");
-			   tgh3.start();
-			   
-			   tgh2= new ThrGen("HOUSE Thread-2");
-			   tgh2.start();
-
-			   tgh5= new ThrGen("HOUSE Thread-5");
-			   tgh5.start();
-
-			   tgh4= new ThrGen("HOUSE Thread-4");
-			   tgh4.start();
-
-			   synchronized(tgh1){
-		            try{
-		                System.out.println("Waiting for HOUSE Thread-1 to complete...");
-		                tgh1.wait();
-		            } catch(InterruptedException e){
-		                e.printStackTrace();
-		            }
-			   }
-			   
-
-			   /*synchronized(tgh2){
-		            try{
-		                System.out.println("Waiting for HOUSE Thread-2 to complete...");
-		                tgh2.wait();
-		            } catch(InterruptedException e){
-		                e.printStackTrace();
-		            }
-			   }*/
-
-			   
-			   synchronized(tgh3){
-		            try{
-		                System.out.println("Waiting for HOUSE Thread-3 to complete...");
-		                tgh3.wait();
-		            } catch(InterruptedException e){
-		                e.printStackTrace();
-		            }
-			       }
-
-			   synchronized(tgh5){
-		            try{
-		                System.out.println("Waiting for HOUSE Thread-5 to complete...");
-		                tgh5.wait();
-		            } catch(InterruptedException e){
-		                e.printStackTrace();
-		            }
-	     		   }
-		   
-			   synchronized(tgh4){
-		            try{
-		                System.out.println("Waiting for HOUSE Thread-4 to complete...");
-		                tgh4.wait();
-		            } catch(InterruptedException e){
-		                e.printStackTrace();
-		            }
+			   SrvThr srv = new SrvThr(10);
+			   System.out.println("complete!");
 			   }
 
 			   System.out.println("Check");
-		   
-		   }
-		   
-		
 		   
 		   System.out.println("Thread ThrMain exiting.");
 	   }
--- Direct/src/main/java/com/direct/webflow/WebCtrl.java
+++ Direct/src/main/java/com/direct/webflow/WebCtrl.java
@@ -1,5 +1,6 @@
 package com.direct.webflow;
 
+import java.sql.CallableStatement;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
@@ -12,6 +13,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/*import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+*/
 /*
 @Configuration
 @EnableWebMvc*/
@@ -59,6 +78,16 @@ public class WebCtrl {
 	@RequestMapping("/startGen")
     @ResponseBody
     String startGen() {
+		
+		
+		List<TempObj> tl = new TempObjDao().findAll();
+		for (TempObj itm : tl) {
+			
+	           System.out.println("OBJECT:"+itm.getId());
+			
+		}
+		
+		
     	if (T1 == null || T1.stopped) {
    	  	   T1= new ThrMain(new SprGenItmDao().findAllChecked());
    	  	   T1.start();
