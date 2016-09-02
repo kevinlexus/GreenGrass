3b861740cd307f9b45941612f87b488a07e0235e
lev <factor@mail.ru>
2016-09-02 10:11:10 +0600

    Сделаны изменения для работы Spring Boot как Windows сервиса

--- BillWeb/src/main/java/com/ric/web/BillWebApplication.java
+++ BillWeb/src/main/java/com/ric/web/BillWebApplication.java
@@ -1,15 +1,38 @@
 package com.ric.web;
 
+import java.lang.management.ManagementFactory;
+
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
 @EnableCaching
 public class BillWebApplication {
 
-	public static void main(String[] args) {
+	private static ApplicationContext applicationContext = null;
+	
+	/*public static void main(String[] args) {
 		SpringApplication.run(BillWebApplication.class, args);
-	}
+	}*/
+	
+	public static void main(String[] args) {
+        String mode = args != null && args.length > 0 ? args[0] : null;
+
+        if (applicationContext != null && mode != null && "stop".equals(mode)) {
+            System.exit(SpringApplication.exit(applicationContext, new ExitCodeGenerator() {
+                @Override
+                public int getExitCode() {
+                    return 0;
+                }
+            }));
+        }
+        else {
+            SpringApplication app = new SpringApplication(BillWebApplication.class);
+            applicationContext = app.run(args);
+        }
+    }	
 }
