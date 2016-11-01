1f3fd4d3daa693c69038e0775f03bd71b4cd951e
lev <factor@mail.ru>
2016-11-01 15:45:29 +0600

    исправление

--- SoapThrower/src/main/java/com/ric/st/impl/LoggingSOAPHandler.java
+++ SoapThrower/src/main/java/com/ric/st/impl/LoggingSOAPHandler.java
@@ -2,12 +2,13 @@ package com.ric.st.impl;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
-import javax.xml.soap.Node;
 import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPHeader;
@@ -20,8 +21,13 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+import ru.gosuslugi.dom.signature.demo.args.SignParameters;
+import ru.gosuslugi.dom.signature.demo.commands.Command;
+import ru.gosuslugi.dom.signature.demo.commands.SignCommand;
 
 public class LoggingSOAPHandler implements SOAPHandler<SOAPMessageContext> {
 
@@ -43,79 +49,42 @@ public class LoggingSOAPHandler implements SOAPHandler<SOAPMessageContext> {
 		try {
 			soapEnv = soapMsg.getSOAPPart().getEnvelope();
 	        SOAPHeader soapHeader = soapEnv.getHeader();
-	        
 	        SOAPBody soapBody = soapMsg.getSOAPBody();
 
 	        XPath xpath = XPathFactory.newInstance().newXPath();
-	        try {
-				Node resultNode = (Node) xpath.evaluate("//*[local-name() = 'BasicCharacteristicts']", soapMsg.getSOAPBody(), XPathConstants.NODE);
-				System.out.println("1="+resultNode);
-				System.out.println("2="+resultNode.getLocalName());
-		        
-				--- и начать здесь подписывание!
-				Element et = (Element) resultNode;
-		        
-		        System.out.println("3="+et.getTextContent());
-		        
-				//resultNode.setTextContent("VBBBBBBBBBBBBBBBBBBBBBBB");
-			} catch (XPathExpressionException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-
-	        //soapMsg.saveChanges();
-
-	        
-	        /*	        
-	        NodeList nodeLst = soapMsg
-	                                .getSOAPBody()
-	                                .getElementsByTagName("*");
-
-	        int len = nodeLst.getLength();
-	        for(int i = 0; i < len; i++) {
-	        	if (nodeLst.item(i) != null) {
-			        System.out.println("Check!!!"+nodeLst.item(i).getTextContent());
-			        //nodeLst.item(i).setTextContent("bla bla");
-			        System.out.println("1"+nodeLst.item(i).getLocalName());
-			        System.out.println("2"+nodeLst.item(i).getNodeName());
-			        System.out.println("3"+nodeLst.item(i).getNodeValue());
-			        
-	        	}
-	            //credentials.item(i).setTextContent("new credential content goes here...");
-	            //credentials.item(i).setTextContent("new credential content goes here...");
-	        }	        
-	        soapMsg.saveChanges();
-	        */
-	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	        try {
-				soapMsg.writeTo(baos);
+	        Command sc = new SignCommand(null);
+			
+			ByteArrayOutputStream bs = new ByteArrayOutputStream();
+			try {
+				soapMsg.writeTo(bs);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	        
-	        System.out.println("CHECK IT:"+baos.toString());
-	        
-	        //soapMsg.
-	        
-	        //soapBody.writeTo(System.out);
-	        
-/*	        java.util.Iterator iterator = soapBody.getChildElements();
-	        while (iterator.hasNext()) {
-	          SOAPBodyElement bodyElement = (SOAPBodyElement) iterator.next();
-	          String val = bodyElement.getValue();
-	          System.out.println("The Value is:" + val);
-	        }*/	        	
+			try {
+				ThrowerMng.xmlText = sc.signElem(bs.toString(), "foo", "foo");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-	        soapMsg.saveChanges();
-            
-			//soapMsg.writeTo(System.out);
-		} catch (SOAPException e) {
+	        //soapMsg.saveChanges();
+
+/*		System.out.println("BEGIN MSG:");
+	        try {
+				soapMsg.writeTo(System.out);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+*/		} catch (SOAPException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("CHECCCCCCCCCK!");
-		return true;
+
+/*		System.out.println("END MSG:");
+*/		
+		return false;
 	}
 
 	public Set<QName> getHeaders() {
